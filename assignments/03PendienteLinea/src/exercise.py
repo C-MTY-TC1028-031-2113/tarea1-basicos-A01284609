45b2f4f8597220d367e9ce64ae7b0ad637160ed4
A01284609 <89152508+A01284609@users.noreply.github.com>
2021-08-22 21:40:20 -0500

    22/08/2021

--- assignments/03PendienteLinea/src/exercise.py
+++ assignments/03PendienteLinea/src/exercise.py
@@ -1,7 +1,10 @@
 def main():
-    #escribe tu código abajo de esta línea
-    #Lee los datos
-    pass
+    x1 = float(input("Dame x1: "))
+    y1 = float(input("Dame y1: "))
+    x2 = float(input("Dame x2: "))
+    y2 = float(input("Dame y2: "))
+    pendiente = str((y2 - y1) / (x2 - x1))
+    print("Pendiente: " + str(pendiente))
 
 
 
